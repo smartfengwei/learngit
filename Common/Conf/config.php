dcf252b2776877802255a645ded2695f03cf9148
FengWei <smartfengwei@163.com>
2017-01-11 14:45:32 +0800

    modify dog and cat

---
+++ Common/Conf/config.php
@@ -0,0 +1,18 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	//数据库配置信息
+	'DB_TYPE'   => 'mysql', // 数据库类型
+	'DB_HOST'   => 'localhost', // 服务器地址
+	'DB_NAME'   => '2cyj', // 数据库名
+	'DB_USER'   => 'root', // 用户名
+	'DB_PWD'    => 'root', // 密码
+	'DB_PORT'   => 3306, // 端口
+	'DB_PREFIX' => '', // 数据库表前缀 
+	'DB_CHARSET'=> 'utf8', // 字符集
+
+	//路由配置
+	'DEFAULT_MODULE'     => 'Index', //默认模块    
+	'URL_MODEL'          => '2', //URL模式 
+	'SESSION_AUTO_START' => true, //是否开启session
+);
